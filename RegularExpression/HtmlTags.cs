fcaeb9246614b3276be26a25cf8a4a17797976e7
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-10-06 14:45:17 +0530

    adding practice problems on regular expression

---
+++ RegularExpression/HtmlTags.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace RegularExpression
+{
+    internal class HtmlTags
+    {
+        public void Tags()
+        {
+            string input = "< p > The < code > Regex </ code > is a compiledrepresentation of a regular expression.</ p >";
+            string pattern = "<[^>]+>";//"^[<][a-z]{1,}[>][<][a-z]{1,}[/>]$";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
